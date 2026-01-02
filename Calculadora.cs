3471e121e6665fedc46567301aa032a5848b9a93
Tiago Aguiar Moreira <tiago.aguiar1@hotmail.com>
2026-01-02 13:12:52 -0300

    remove soma

--- Calculadora.cs
+++ Calculadora.cs
@@ -2,11 +2,6 @@ namespace MinhaApp;
 
 public class Calculadora
 { 
-    public int Somar(int a, int b)
-    {
-        return a + b;
-    }
-
     public int Subtrair(int a, int b)
     {
         return a - b;
