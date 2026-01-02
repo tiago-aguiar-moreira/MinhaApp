1a24636da63a0cfa6d29b0b53248c3ccb01ea5f2
Tiago Aguiar Moreira <tiago.aguiar1@hotmail.com>
2026-01-02 11:32:43 -0300

    Primeiro versionamento

--- Calculadora.cs
+++ Calculadora.cs
@@ -1,9 +1,14 @@
 namespace MinhaApp;
 
 public class Calculadora
-{
+{ 
     public int Somar(int a, int b)
     {
         return a + b;
+    }
+
+    public int Subtrair(int a, int b)
+    {
+        return a - b;
     }
 }
