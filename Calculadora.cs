b350171af3c2599caf7ab5b30bba0306d2c098c0
Tiago Aguiar Moreira <tiago.aguiar1@hotmail.com>
2026-01-02 13:23:04 -0300

    remove subtração

--- Calculadora.cs
+++ Calculadora.cs
@@ -2,11 +2,6 @@ namespace MinhaApp;
 
 public class Calculadora
 { 
-    public int Subtrair(int a, int b)
-    {
-        return a - b;
-    }
-
     public int Multiplicar(int a, int b)
     {
         return a * b;
